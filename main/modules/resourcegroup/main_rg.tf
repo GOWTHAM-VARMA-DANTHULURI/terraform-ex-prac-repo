2a9d1592ee37bcb6de9a7c7585488c368c60d39c
GOWTHAM-VARMA-DANTHULURI <60300099+GOWTHAM-VARMA-DANTHULURI@users.noreply.github.com>
2022-10-22 00:36:04 +0100

    created resource group and storage account through modules

---
+++ main/modules/resourcegroup/main_rg.tf
@@ -0,0 +1,6 @@
+resource "azurerm_resource_group" "resource_group" {
+    name = local.rg_name
+    location = var.rg-location
+    tags = var.tags
+  
+}
---
+++ main/modules/storage/st_variables.tf
@@ -0,0 +1,31 @@
+variable "location" {
+    description = "Location where the storage resource is created"
+    default = "uksouth"
+}
+
+variable "account_tier" {
+  default = "Standard"
+}
+
+variable "account_replication_type" {
+    default = "GRS"
+}
+
+variable "tags" {
+    description = "Tags for the storage resource"
+    type = map(string)
+    default = {
+        environment = "dev"
+    }
+  
+}
+
+variable "resource_group_name" {
+  default = ""
+}
+
+variable "storage_name" {
+  description = "name of the storage account"
+  default = "mychutki0002"
+  
+}
