546bf13e1c8509c6f3b9a5fa30b90fbb1f714841
GOWTHAM-VARMA-DANTHULURI <60300099+GOWTHAM-VARMA-DANTHULURI@users.noreply.github.com>
2022-10-24 01:27:28 +0100

    Created a ResourceGroup, KeyVault, KeyVaultSecrets &  AKS-Cluster.

--- main/modules/resourcegroup/main_rg.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_resource_group" "resource_group" {
-    name = local.rg_name
-    location = var.rg-location
-    tags = var.tags
-  
-}
---
+++ modules/keyvaultsecret/kvs_vars.tf
@@ -0,0 +1,23 @@
+variable "secret_name" {
+  description = "KV secret name"
+  default     = "gowtham-secret-name"
+  type        = string
+}
+
+variable "secret_names" {
+  description = "KV secret names - list"
+  type        = map(string)
+}
+
+variable "secret_value" {
+  description = "KV secret value"
+  default     = "gowtham-secret-value"
+  type        = string
+}
+
+variable "key_vault_id" {
+  description = "Default application name in short form."
+  default     = "keyvault_id"
+  type        = string
+}
+
